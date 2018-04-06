98ce75c2d8af5802c6ba268e554caa668d8c6a91
moennen <nicolas.moenne-loccoz@autodesk.com>
2018-04-06 07:27:12 -0400

    Test map creation

--- depthMapCreation/depthMapCreation_main.cpp
+++ depthMapCreation/depthMapCreation_main.cpp
@@ -19,9 +19,9 @@
 using namespace glm;
 
 // opencv
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
+#include "utils/cv_utils.h"
+
+#include <memory>
 
 #include <iostream>
 
@@ -33,168 +33,362 @@ const string keys =
     "{help h usage ? |         | print this message   }"
     "{@imageA        |         | image1 for compare   }";
 
-static ivec2 windowSz(1024, 768);
+static ivec2 windowSz( 1024, 768 );
 
-void init() {
-  glClearColor(0.0, 0.0, 0.0, 0.0);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0.0, windowSz.x, windowSz.y, 1.0, -1.0, 1.0);
-  glEnable(GL_BLEND);
-  glEnable(GL_TEXTURE_2D);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-}
-void draw(GLuint tex, size_t w, size_t h) {
-  // Bind Texture
-  glBindTexture(GL_TEXTURE_2D, tex);
+#define INPUTA_MODE 0
+#define MAP_MODE 2
 
-  GLfloat Vertices[] = {(float)0,
-                        (float)0,
-                        0,
-                        (float)0 + w,
-                        (float)0,
-                        0,
-                        (float)0 + (float)w,
-                        (float)0 + (float)h,
-                        0,
-                        (float)0,
-                        (float)0 + (float)h,
-                        0};
-  GLfloat TexCoord[] = {
-      0, 0, 1, 0, 1, 1, 0, 1,
-  };
-  GLubyte indices[] = {
-      0, 1,
-      2,         // first triangle (bottom left - top left - top right)
-      0, 2, 3};  // second triangle (bottom left - top right - bottom right)
+using MapFunc = PhSpline<2, 3, 2>;
+using MapFuncPtr = unique_ptr<MapFunc>;
 
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glVertexPointer(3, GL_FLOAT, 0, Vertices);
+MapFuncPtr createNewMapFunc( const vector<vec2>& mapCtrlPtx, const Mat& input )
+{
+   vector<vec2> mapCtrlPos;
+   mapCtrlPos.reserve( mapCtrlPtx.size() );
+   vector<vec3> mapCtrlValues;
+   mapCtrlValues.reserve( mapCtrlPtx.size() );
+#pragma omp parallel for
+   for ( size_t idx = 0; idx < mapCtrlPtx.size(); ++idx )
+   {
+      const auto ctrVal = cv_utils::imsample32FC3( input, mapCtrlPtx[idx] );
+      mapCtrlPos.emplace_back(
+          vec2( 2.0f ) * ( mapCtrlPtx[idx] / vec2( windowSz ) - vec2( 0.5f ) ) );
+      mapCtrlValues.emplace_back( ctrVal( 0 ), ctrVal( 1 ), ctrVal( 2 ) );
+   }
 
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  glTexCoordPointer(2, GL_FLOAT, 0, TexCoord);
-
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
-
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  glDisableClientState(GL_VERTEX_ARRAY);
+   return MapFuncPtr( new MapFunc(
+       value_ptr( mapCtrlPos[0] ), value_ptr( mapCtrlValues[0] ), mapCtrlPtx.size() ) );
 }
 
-int main(int argc, char* argv[]) {
-  CommandLineParser parser(argc, argv, keys);
-  if (parser.has("help")) {
-    parser.printMessage();
-    return (0);
-  }
-  string inputFilenameA = parser.get<string>("@imageA");
+void computeMap( const MapFunc& mapFunc, const vector<vec2>& mapCtrlPtx, Mat& map )
+{
+   vector<vec2> mapCtrlPos;
+   mapCtrlPos.reserve( mapCtrlPtx.size() );
+   for ( size_t idx = 0; idx < mapCtrlPtx.size(); ++idx )
+   {
+      mapCtrlPos.emplace_back(
+          vec2( 2.0f ) * ( mapCtrlPtx[idx] / vec2( windowSz ) - vec2( 0.5f ) ) );
+   }
 
-  /*auto func = [](const vec2 x) {
-    return exp(0.5 * x.x * x.x) * exp(0.5 * x.y * x.y);
-  };
-  vector<vec2> ctrlX = {
-      {0.3f, -0.1f}, {-0.04f, 0.8f}, {-0.97f, 0.001f}, {0.75f, -1.3f}};
-  vector<float> ctrlY;
-  ctrlY.reserve(ctrlX.size());
-  transform(ctrlX.cbegin(), ctrlX.cend(), std::back_inserter(ctrlY), func);
-
-  PhSpline<2, 1, 2, float> interpFunc(value_ptr(ctrlX[0]), &ctrlY[0],
-                                      ctrlX.size());
-
-  for (float x = -2.0f; x <= 2.0f; x += 0.01f) {
-    // for (float y = -1.0f; y <= 1.0f; y += 0.1f) {
-    const vec2 xy(x, 0.0f);
-    const float rf = func(xy);
-    float ef;
-    interpFunc(value_ptr(ctrlX[0]), value_ptr(xy), &ef);
-    cout << x << " " << rf << " " << ef << endl;
-  }
-  //}*/
-
-  auto func = [](const float x) { return x < 0.0f ? 0.0f : exp(0.5 * x * x); };
-  vector<float> ctrlX = {-2.0f, 1.0f,   1.5f, 0.3f,   -0.1f, 0.1f, 0.5f,
-                         -0.6f, -0.04f, 0.8f, -1.75f, -1.3f, 2.0f};
-  vector<float> ctrlY;
-  ctrlY.reserve(ctrlX.size());
-  transform(ctrlX.cbegin(), ctrlX.cend(), std::back_inserter(ctrlY), func);
-
-  PhSpline<1, 1, 2, float> interpFunc(&ctrlX[0], &ctrlY[0], ctrlX.size());
-
-  for (float x = -2.0f; x <= 2.0f; x += 0.01f) {
-    const float rf = func(x);
-    float ef;
-    interpFunc(&ctrlX[0], &x, &ef);
-    cout << x << " " << rf << " " << ef << endl;
-  }
-
-  // SDL init
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window* window = SDL_CreateWindow("shLighting", SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED, windowSz.x,
-                                        windowSz.y, SDL_WINDOW_OPENGL);
-  SDL_GLContext glCtx = SDL_GL_CreateContext(window);
-
-  // load and setup the input image
-  cv::Mat inputA = cv::imread(inputFilenameA.c_str());
-  if (inputA.type() != CV_32F) {
-    inputA.convertTo(inputA, CV_32F);
-    inputA *= 1.0 / 255.0;
-  }
-  cv::cvtColor(inputA, inputA, cv::COLOR_BGR2RGB);
-
-  //
-  gl_utils::Texture<gl_utils::RGB_32FP> texA(
-      glm::uvec2(inputA.cols, inputA.rows));
-  gl_utils::uploadToTexture(texA, inputA.ptr());
-
-  cv::Mat output(texA.sz.x, texA.sz.y, CV_32FC3);
-  gl_utils::readbackTexture(texA, output.ptr());
-
-  bool running = true;
-  Uint32 start;
-  SDL_Event event;
-
-  init();
-  while (running) {
-    start = SDL_GetTicks();
-    draw(texA.id, texA.sz.x, texA.sz.y);
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          running = false;
-          break;
-        case SDL_MOUSEBUTTONDOWN:
-          switch (event.button.button) {
-            case SDL_BUTTON_LEFT:
-              break;
-            case SDL_BUTTON_RIGHT:
-              break;
-            default:
-              break;
-          }
-          break;
-        case SDL_MOUSEMOTION: {
-          int mouseX = event.motion.x;
-          int mouseY = event.motion.y;
-          std::stringstream ss;
-          ss << "X: " << mouseX << " Y: " << mouseY;
-          SDL_SetWindowTitle(window, ss.str().c_str());
-        } break;
-        case SDL_MOUSEWHEEL:
-          if (event.wheel.y == 1)  // scroll up
-          {
-            // Pull up code here!
-          } else if (event.wheel.y == -1)  // scroll down
-          {
-            // Pull down code here!
-          }
-          break;
+   const auto ctrlPtx = value_ptr( mapCtrlPos[0] );
+#pragma omp parallel for
+   for ( size_t y = 0; y < map.rows; y++ )
+   {
+      float* row_data = map.ptr<float>( y );
+      vec3 val;
+      for ( size_t x = 0; x < map.cols; x++ )
+      {
+         mapFunc(
+             ctrlPtx,
+             value_ptr( vec2( 2.0f ) * ( vec2( x, y ) / vec2( windowSz ) - vec2( 0.5f ) ) ),
+             value_ptr( val ) );
+         row_data[x * 3 + 0] = val.x;
+         row_data[x * 3 + 1] = val.y;
+         row_data[x * 3 + 2] = val.z;
       }
-    }
-    SDL_GL_SwapWindow(window);
-    if (1000 / 60 > (SDL_GetTicks() - start))
-      SDL_Delay(1000 / 60 - (SDL_GetTicks() - start));
-  }
-  SDL_Quit();
+   }
+}
 
-  return (0);
+void init()
+{
+   glClearColor( 0.0, 0.0, 0.0, 0.0 );
+   glMatrixMode( GL_PROJECTION );
+   glLoadIdentity();
+   glOrtho( 0.0, windowSz.x, windowSz.y, 1.0, -1.0, 1.0 );
+   glEnable( GL_BLEND );
+   glEnable( GL_TEXTURE_2D );
+   glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+}
+
+size_t getSelectedPt( const vec2 pos, const vector<vec2>& ptx )
+{
+   const float defaultSelRadius = 7.0;
+   for ( size_t i = 0; i < ptx.size(); ++i )
+   {
+      const vec2 pt = {ptx[i].x, ptx[i].y};
+      if ( distance( pt, pos ) <= defaultSelRadius ) return i;
+   }
+   return ptx.size();
+}
+
+void drawCircle( float x, float y, float r, int segments, const vec4& colour )
+{
+   glBegin( GL_TRIANGLE_FAN );
+   glColor4fv( glm::value_ptr( colour * 0.5f ) );
+   glVertex2f( x, y );
+   glColor4fv( value_ptr( colour ) );
+   for ( int n = 0; n <= segments; ++n )
+   {
+      float const t = 2 * M_PI * (float)n / (float)segments;
+      glVertex2f( x + sin( t ) * r, y + cos( t ) * r );
+   }
+   glEnd();
+}
+
+void drawCtrlPtx( const vector<vec2>& ctrlPtx, const size_t sel )
+{
+   const float defaultRadius = 5.0;
+
+   for ( size_t i = 0; i < ctrlPtx.size(); ++i )
+   {
+      const vec2& pt = ctrlPtx[i];
+      const float alpha = 0.85;
+      const vec4 colour =
+          ( i == sel ) ? vec4( 0.75f, 0.0f, 0.0f, alpha ) : vec4( 0.0f, 0.75f, 0.0f, alpha );
+
+      drawCircle( pt.x, pt.y, defaultRadius, 128, colour );
+   }
+   glColor4f( 1.0, 1.0, 1.0, 1.0 );
+}
+
+void draw( GLuint tex, size_t w, size_t h )
+{
+   // Bind Texture
+   glBindTexture( GL_TEXTURE_2D, tex );
+
+   GLfloat Vertices[] = {(float)0,
+                         (float)0,
+                         0,
+                         (float)0 + w,
+                         (float)0,
+                         0,
+                         (float)0 + (float)w,
+                         (float)0 + (float)h,
+                         0,
+                         (float)0,
+                         (float)0 + (float)h,
+                         0};
+   GLfloat TexCoord[] = {
+       0, 0, 1, 0, 1, 1, 0, 1,
+   };
+   GLubyte indices[] = {0,
+                        1,
+                        2,  // first triangle (bottom left - top left - top right)
+                        0,
+                        2,
+                        3};  // second triangle (bottom left - top right - bottom right)
+
+   glEnableClientState( GL_VERTEX_ARRAY );
+   glVertexPointer( 3, GL_FLOAT, 0, Vertices );
+
+   glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+   glTexCoordPointer( 2, GL_FLOAT, 0, TexCoord );
+
+   glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices );
+
+   glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+   glDisableClientState( GL_VERTEX_ARRAY );
+}
+
+int main( int argc, char* argv[] )
+{
+   CommandLineParser parser( argc, argv, keys );
+   if ( parser.has( "help" ) )
+   {
+      parser.printMessage();
+      return ( 0 );
+   }
+   string inputFilenameA = parser.get<string>( "@imageA" );
+
+   /*auto func = [](const vec2 x) {
+     return exp(0.5 * x.x * x.x) * exp(0.5 * x.y * x.y);
+   };
+   vector<vec2> ctrlX = {
+       {0.3f, -0.1f}, {-0.04f, 0.8f}, {-0.97f, 0.001f}, {0.75f, -1.3f}};
+   vector<float> ctrlY;
+   ctrlY.reserve(ctrlX.size());
+   transform(ctrlX.cbegin(), ctrlX.cend(), std::back_inserter(ctrlY), func);
+
+   PhSpline<2, 1, 2, float> interpFunc(value_ptr(ctrlX[0]), &ctrlY[0],
+                                       ctrlX.size());
+
+   for (float x = -2.0f; x <= 2.0f; x += 0.01f) {
+     // for (float y = -1.0f; y <= 1.0f; y += 0.1f) {
+     const vec2 xy(x, 0.0f);
+     const float rf = func(xy);
+     float ef;
+     interpFunc(value_ptr(ctrlX[0]), value_ptr(xy), &ef);
+     cout << x << " " << rf << " " << ef << endl;
+   }
+   //}*/
+
+   /*auto func = []( const float x ) { return x < 0.0f ? 0.0f : exp( 0.5 * x * x ); };
+   vector<float> ctrlX = {
+       -2.0f, 1.0f, 1.5f, 0.3f, -0.1f, 0.1f, 0.5f, -0.6f, -0.04f, 0.8f, -1.75f, -1.3f, 2.0f};
+   vector<float> ctrlY;
+   ctrlY.reserve( ctrlX.size() );
+   transform( ctrlX.cbegin(), ctrlX.cend(), std::back_inserter( ctrlY ), func );
+
+   PhSpline<1, 1, 2, float> interpFunc( &ctrlX[0], &ctrlY[0], ctrlX.size() );
+
+   for ( float x = -2.0f; x <= 2.0f; x += 0.01f )
+   {
+      const float rf = func( x );
+      float ef;
+      interpFunc( &ctrlX[0], &x, &ef );
+      cout << x << " " << rf << " " << ef << endl;
+   }*/
+
+   // load and setup the input image
+   cv::Mat inputA = cv::imread( inputFilenameA.c_str() );
+   if ( inputA.type() != CV_32F )
+   {
+      inputA.convertTo( inputA, CV_32F );
+      inputA *= 1.0 / 255.0;
+   }
+   cv::cvtColor( inputA, inputA, cv::COLOR_BGR2RGB );
+
+   windowSz = glm::ivec2( inputA.cols, inputA.rows );
+
+   // SDL init
+   SDL_Init( SDL_INIT_EVERYTHING );
+   SDL_Window* window = SDL_CreateWindow(
+       "shLighting",
+       SDL_WINDOWPOS_CENTERED,
+       SDL_WINDOWPOS_CENTERED,
+       windowSz.x,
+       windowSz.y,
+       SDL_WINDOW_OPENGL );
+   SDL_GLContext glCtx = SDL_GL_CreateContext( window );
+
+   //
+   gl_utils::Texture<gl_utils::RGB_32FP> texA( glm::uvec2( inputA.cols, inputA.rows ) );
+   gl_utils::uploadToTexture( texA, inputA.ptr() );
+
+   cv::Mat map = cv::Mat::zeros( inputA.rows, inputA.cols, CV_32FC3 );
+   gl_utils::Texture<gl_utils::RGB_32FP> texMap( glm::uvec2( inputA.cols, inputA.rows ) );
+   gl_utils::uploadToTexture( texMap, map.ptr() );
+
+   gl_utils::Texture<gl_utils::RGB_32FP> depthTex( glm::uvec2( inputA.cols, inputA.rows ) );
+   std::vector<glm::vec2> depthCtrlPtx;
+   size_t selectedDepthCtrlPtx = 0;
+
+   // create the map model
+   MapFuncPtr mapFuncPtr;
+
+   bool running = true;
+   int displayMode = INPUTA_MODE;
+   bool updateMap = false;
+   Uint32 start;
+   SDL_Event event;
+
+   init();
+   while ( running )
+   {
+      start = SDL_GetTicks();
+      while ( SDL_PollEvent( &event ) )
+      {
+         switch ( event.type )
+         {
+            case SDL_QUIT:
+               running = false;
+               break;
+            case SDL_KEYDOWN:
+               /* Check the SDLKey values and move change the coords */
+               switch ( event.key.keysym.sym )
+               {
+                  case SDLK_0:
+                     displayMode = INPUTA_MODE;
+                     break;
+                  case SDLK_2:
+                     displayMode = MAP_MODE;
+                     break;
+               }
+            case SDL_MOUSEBUTTONDOWN:
+               switch ( event.button.button )
+               {
+                  case SDL_BUTTON_LEFT:
+                  {
+                     selectedDepthCtrlPtx =
+                         getSelectedPt( glm::vec2( event.motion.x, event.motion.y ), depthCtrlPtx );
+                     const bool hasSelection = selectedDepthCtrlPtx < depthCtrlPtx.size();
+                     // no selection : create a new ctrl pt
+                     if ( !hasSelection )
+                     {
+                        // add
+                        depthCtrlPtx.emplace_back( event.motion.x, event.motion.y );
+                     }
+                     updateMap = true;
+                     // disable mouse cursor
+                     SDL_ShowCursor( SDL_DISABLE );
+                     break;
+                  }
+                  case SDL_BUTTON_RIGHT:
+                     break;
+                  default:
+                     break;
+               }
+               break;
+            case SDL_MOUSEBUTTONUP:
+               switch ( event.button.button )
+               {
+                  case SDL_BUTTON_LEFT:
+                     // release the current selection
+                     selectedDepthCtrlPtx = depthCtrlPtx.size();
+                     // disable mouse cursor
+                     SDL_ShowCursor( SDL_ENABLE );
+                     break;
+                  case SDL_BUTTON_RIGHT:
+                     break;
+                  default:
+                     break;
+               }
+               break;
+            case SDL_MOUSEMOTION:
+            {
+               int mouseX = event.motion.x;
+               int mouseY = event.motion.y;
+               std::stringstream ss;
+               ss << "X: " << mouseX << " Y: " << mouseY;
+               SDL_SetWindowTitle( window, ss.str().c_str() );
+               {
+                  const bool hasSelection = selectedDepthCtrlPtx < depthCtrlPtx.size();
+                  // no selection : create a new ctrl pt
+                  if ( hasSelection )
+                  {
+                     depthCtrlPtx[selectedDepthCtrlPtx].x = mouseX;
+                     depthCtrlPtx[selectedDepthCtrlPtx].y = mouseY;
+                     updateMap = true;
+                  }
+               }
+            }
+            break;
+            case SDL_MOUSEWHEEL:
+               if ( event.wheel.y == 1 )  // scroll up
+               {
+                  // Pull up code here!
+               }
+               else if ( event.wheel.y == -1 )  // scroll down
+               {
+                  // Pull down code here!
+               }
+               break;
+         }
+      }
+      if ( updateMap && ( displayMode == MAP_MODE ) )
+      {
+         mapFuncPtr = createNewMapFunc( depthCtrlPtx, inputA );
+         computeMap( *mapFuncPtr.get(), depthCtrlPtx, map );
+         gl_utils::uploadToTexture( texMap, map.ptr() );
+         updateMap = false;
+      }
+      glClear( GL_COLOR_BUFFER_BIT );
+      if ( displayMode == INPUTA_MODE )
+      {
+         draw( texA.id, texA.sz.x, texA.sz.y );
+      }
+      else
+      {
+         draw( texMap.id, texMap.sz.x, texMap.sz.y );
+      }
+      // updateDepthTextureFromCtrlPtx( depthTex, depthCtrlPtx );
+      // draw( depthTex.id, depthTex.sz.x, depthTex.sz.y );
+      drawCtrlPtx( depthCtrlPtx, selectedDepthCtrlPtx );
+      SDL_GL_SwapWindow( window );
+      if ( 1000 / 60 > ( SDL_GetTicks() - start ) )
+         SDL_Delay( 1000 / 60 - ( SDL_GetTicks() - start ) );
+   }
+   SDL_Quit();
+
+   return ( 0 );
 }
--- utils/phSpline.h
+++ utils/phSpline.h
@@ -11,98 +11,107 @@
 #include <Eigen/Dense>
 #include <vector>
 
-template <unsigned InDim = 2, unsigned OutDim = InDim, unsigned Order = 2,
-          typename Real = float>
-class PhSpline final {
-  const size_t _nCtrlPts;
-  // Params of the spline : OutDim x ( _nCtrPts ( weights )  + InDim + 1  (
-  // affine ) )
-  std::vector<Real> _params;
+template <unsigned InDim = 2, unsigned OutDim = InDim, unsigned Order = 2, typename Real = float>
+class PhSpline final
+{
+   const size_t _nCtrlPts;
+   // Params of the spline : OutDim x ( _nCtrPts ( weights )  + InDim + 1  (
+   // affine ) )
+   std::vector<Real> _params;
 
-  template <typename t, int dim = Eigen::Dynamic>
-  using Vector = Eigen::Matrix<t, dim, 1>;
-  using Matrix = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>;
+   template <typename t, int dim = Eigen::Dynamic>
+   using Vector = Eigen::Matrix<t, dim, 1>;
+   using Matrix = Eigen::Matrix<Real, Eigen::Dynamic, Eigen::Dynamic>;
 
- public:
-  PhSpline(const Real* ctrlPts, const Real* ctrlFunc, const size_t nCtrlPts)
-      : _nCtrlPts(nCtrlPts), _params(OutDim * (_nCtrlPts + InDim + 1)) {
-    // reject unsolvable system
-    assert(_nCtrlPts > InDim);
-    fit(ctrlPts, ctrlFunc, _nCtrlPts, &_params[0]);
-  }
+  public:
+   PhSpline( const Real* ctrlPts, const Real* ctrlFunc, const size_t nCtrlPts )
+       : _nCtrlPts( nCtrlPts ), _params( OutDim * ( _nCtrlPts + InDim + 1 ) )
+   {
+      // reject unsolvable system
+      assert( _nCtrlPts > InDim );
+      fit( ctrlPts, ctrlFunc, _nCtrlPts, &_params[0] );
+   }
 
-  // given a point x of InDim dimensions return the corresponding function value
-  // y
-  // in OutDim dimensions
-  inline void operator()(const Real* ctrlPts, const Real* x, Real* y) const {
-    using namespace Eigen;
-    const size_t nParams = _nCtrlPts + InDim + 1;
-    const auto mapX = Map<const Vector<Real, InDim> >(x);
-    const auto mapCtrlPts = Map<const Matrix>(ctrlPts, InDim, _nCtrlPts);
-    Vector<Real> vecX(nParams);
-    for (unsigned pt = 0; pt < _nCtrlPts; ++pt) {
-      vecX[pt] = psi((mapX - mapCtrlPts.col(pt)).norm());
-    }
-    vecX.template segment<InDim>(_nCtrlPts) = mapX;
-    vecX[nParams - 1] = 1.0;
-
-    // compute the interpolated function value
-    auto vecY = Map<Vector<Real, OutDim> >(y);
-    vecY = vecX.adjoint() * Map<const Matrix>(&_params[0], nParams, OutDim);
-  }
-
-  inline size_t getNParams() const { return _nCtrlPts + InDim + 1; }
-  inline const Real* getParams() const & { return &_params[0]; }
-
- private:
-  static inline Real psi(Real r) {
-    return (Order % 2)
-               ? std::pow(r, Order)
-               : r < 1.0f ? std::pow(r, Order - 1) * std::log(std::pow(r, r))
-                          : std::pow(r, Order) * std::log(r);
-  }
-
-  // estimate the weight st
-  // _w = argmin_w ( ||eval(ref,w,ctrl) - probe||^2 )
-  static inline void fit(const Real* ctrlPts, const Real* ctrlFuncs,
-                         const size_t nCtrlPts, Real* params) {
-    using namespace Eigen;
-    // system matrix
-    // -->
-    const size_t nParams = nCtrlPts + InDim + 1;
-    Matrix S(nParams, nParams);
-    //#pragma omp parallel for
-    for (size_t r = 0; r < nCtrlPts; ++r) {
-      auto rx = Map<const Vector<Real, InDim> >(&ctrlPts[r * InDim]);
-      for (unsigned c = 0; c < nCtrlPts; ++c) {
-        auto cx = Map<const Vector<Real, InDim> >(&ctrlPts[c * InDim]);
-        S(r, c) = psi((rx - cx).norm());
+   // given a point x of InDim dimensions return the corresponding function value
+   // y
+   // in OutDim dimensions
+   inline void operator()( const Real* ctrlPts, const Real* x, Real* y ) const
+   {
+      using namespace Eigen;
+      const size_t nParams = _nCtrlPts + InDim + 1;
+      const auto mapX = Map<const Vector<Real, InDim> >( x );
+      const auto mapCtrlPts = Map<const Matrix>( ctrlPts, InDim, _nCtrlPts );
+      Vector<Real> vecX( nParams );
+      for ( unsigned pt = 0; pt < _nCtrlPts; ++pt )
+      {
+         vecX[pt] = psi( ( mapX - mapCtrlPts.col( pt ) ).norm() );
       }
-    }
-    //#pragma omp parallel for
-    for (size_t c = 0; c < nCtrlPts; ++c) {
-      auto cx = Map<const Vector<Real, InDim> >(&ctrlPts[c * InDim]);
-      S.col(c).template segment<InDim>(nCtrlPts) = cx;
-      S.row(c).template segment<InDim>(nCtrlPts) = cx;
-    }
-    S.template block<InDim + 1, InDim + 1>(nCtrlPts, nCtrlPts).setZero();
-    S.col(nParams - 1).topRows(nCtrlPts).setOnes();
-    S.row(nParams - 1).leftCols(nCtrlPts).setOnes();
+      vecX.template segment<InDim>( _nCtrlPts ) = mapX;
+      vecX[nParams - 1] = 1.0;
 
-    Matrix StS(nParams, nParams);
-    StS.template triangularView<Lower>() = S.transpose() * S;
+      // compute the interpolated function value
+      auto vecY = Map<Vector<Real, OutDim> >( y );
+      vecY = vecX.adjoint() * Map<const Matrix>( &_params[0], nParams, OutDim );
+   }
 
-    Vector<Real> f(nParams);
-    f.template bottomRows<InDim + 1>().setZero();
-    for (size_t d = 0; d < OutDim; ++d) {
-      for (size_t c = 0; c < nCtrlPts; ++c) {
-        f[c] = ctrlFuncs[c * OutDim + d];
+   inline size_t getNParams() const { return _nCtrlPts + InDim + 1; }
+   inline const Real* getParams() const & { return &_params[0]; }
+
+  private:
+   static inline Real psi( Real r )
+   {
+      return ( Order % 2 ) ? std::pow( r, Order )
+                           : r < 1.0f ? std::pow( r, Order - 1 ) * std::log( std::pow( r, r ) )
+                                      : std::pow( r, Order ) * std::log( r );
+   }
+
+   // estimate the weight st
+   // _w = argmin_w ( ||eval(ref,w,ctrl) - probe||^2 )
+   static inline void
+   fit( const Real* ctrlPts, const Real* ctrlFuncs, const size_t nCtrlPts, Real* params )
+   {
+      using namespace Eigen;
+      // system matrix
+      // -->
+      const size_t nParams = nCtrlPts + InDim + 1;
+      Matrix S( nParams, nParams );
+      //#pragma omp parallel for
+      for ( size_t r = 0; r < nCtrlPts; ++r )
+      {
+         auto rx = Map<const Vector<Real, InDim> >( &ctrlPts[r * InDim] );
+         for ( unsigned c = 0; c < nCtrlPts; ++c )
+         {
+            auto cx = Map<const Vector<Real, InDim> >( &ctrlPts[c * InDim] );
+            S( r, c ) = psi( ( rx - cx ).norm() );
+         }
       }
-      Matrix Stf = S.transpose() * f;
-      StS.ldlt().solveInPlace(Stf);
-      Map<Vector<Real> >(&params[d * nParams], nParams) = Stf;
-    }
-  }
+      //#pragma omp parallel for
+      for ( size_t c = 0; c < nCtrlPts; ++c )
+      {
+         auto cx = Map<const Vector<Real, InDim> >( &ctrlPts[c * InDim] );
+         S.col( c ).template segment<InDim>( nCtrlPts ) = cx;
+         S.row( c ).template segment<InDim>( nCtrlPts ) = cx;
+      }
+      S.template block<InDim + 1, InDim + 1>( nCtrlPts, nCtrlPts ).setZero();
+      S.col( nParams - 1 ).topRows( nCtrlPts ).setOnes();
+      S.row( nParams - 1 ).leftCols( nCtrlPts ).setOnes();
+
+      Matrix StS( nParams, nParams );
+      StS.template triangularView<Lower>() = S.transpose() * S;
+
+      Vector<Real> f( nParams );
+      f.template bottomRows<InDim + 1>().setZero();
+      for ( size_t d = 0; d < OutDim; ++d )
+      {
+         for ( size_t c = 0; c < nCtrlPts; ++c )
+         {
+            f[c] = ctrlFuncs[c * OutDim + d];
+         }
+         Matrix Stf = S.transpose() * f;
+         StS.ldlt().solveInPlace( Stf );
+         Map<Vector<Real> >( &params[d * nParams], nParams ) = Stf;
+      }
+   }
 };
 
 #endif  // _UTILS_PHSPLINE_H
